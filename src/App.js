fdde819a90fefcc8c4d4a7d2a7d7ba6b8a8a4a89
Nacho <nachoherrero93@hotmail.com>
2020-08-18 14:19:55 -0300

    Fix App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
 import Controller from './Components/Controller';
-// FIXME refactorizar componentes
 
 class App extends React.Component {
   constructor(props) {
